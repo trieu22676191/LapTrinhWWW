89bd22a4044f9128cbdab212c90dff66557ecab8
trieu22676191 <trieu22676191@gmail.com>
2025-09-06 21:16:53 +0700

    Bai 5 tuan 2

---
+++ Tuan2_NguyenPhuTrieu_22676191/Bai5_RestAPI/src/main/java/iuh/fit/se/jakartarestapi/UserApplication.java
@@ -0,0 +1,9 @@
+package iuh.fit.se.jakartarestapi;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class UserApplication extends Application {
+
+}
