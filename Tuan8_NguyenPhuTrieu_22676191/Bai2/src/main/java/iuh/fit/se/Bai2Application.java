96cfbd6e6d73225a5f140d28e7e54751102cf632
trieu22676191 <trieu22676191@gmail.com>
2025-10-26 20:01:44 +0700

    Tuan 8 Bai 2

--- Tuan8_NguyenPhuTrieu_22676191/Bai2/src/main/java/iuh/fit/se/Bai2Application.java
+++ Tuan8_NguyenPhuTrieu_22676191/Bai2/src/main/java/iuh/fit/se/Bai2Application.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Bai1Application {
+public class Bai2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Bai1Application.class, args);
+        SpringApplication.run(Bai2Application.class, args);
     }
+
 }
